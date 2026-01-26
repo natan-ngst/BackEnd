8a322a346ee2d1cf22d61ea0151b9468ccfa2d26
NATANIEL RATUWONGO <NATANIEL RATUWONGO@LAPTOP-3FANOUB2>
2026-01-26 14:43:46 +0800

    segitiga pe luas

---
+++ Segitiga.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Segitiga {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input alas
+        System.out.print("Masukkan alas segitiga: ");
+        double alas = input.nextDouble();
+
+        // Input tinggi
+        System.out.print("Masukkan tinggi segitiga: ");
+        double tinggi = input.nextDouble();
+
+        // Perhitungan luas
+        double luas = 0.5 * alas * tinggi;
+
+        // Output hasil
+        System.out.println("Luas segitiga adalah: " + luas);
+
+
+    }
+}
